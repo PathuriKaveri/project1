51aff3707457a7d0cb9dde768a6532d6eb085183
PathuriKaveri <156990953+PathuriKaveri@users.noreply.github.com>
2024-01-18 11:30:43 +0530

    Update kaveri333.py

--- kaveri333.py
+++ kaveri333.py
@@ -1,1 +1,7 @@
-
+x=(1,2,3,4,5)
+try:
+  print("second element of list=%d" %(x[1])
+  print("fourth element of list=%d" %(x[3])
+  print("sixth element of list=%d" %(x[5])
+except:
+        print("error occured")
